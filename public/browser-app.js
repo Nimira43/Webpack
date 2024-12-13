d93ff1ac8eef0a6ecc7278b4c4a6dd3d32968d72
Nimira43 <n.rabone72@aol.com>
2024-12-13 23:21:38 +0000

    Skeleton function showTasks which'll load tasks from API

--- public/browser-app.js
+++ public/browser-app.js
@@ -3,3 +3,7 @@ const loadingDOM = document.querySelector('loading-text')
 const formDOM = document.querySelector('task-form')
 const taskInputDOM = document.querySelector('task-input')
 const formAlertDOM = document.querySelector('form-alert')
+
+const showTasks = async () => {}
+
+showTasks()
