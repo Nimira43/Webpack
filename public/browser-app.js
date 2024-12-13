7012a10422210fcb342873cbab0d87d3b8c2efd3
Nimira43 <n.rabone72@aol.com>
2024-12-13 23:54:50 +0000

    Code for deleting tasks

--- public/browser-app.js
+++ public/browser-app.js
@@ -44,7 +44,18 @@ const showTasks = async () => {
 showTasks()
 
 tasksDOM,addEventListener('click', async (e) => {
-
+  const el =  e.target
+  if (el.parentElement.classList.contains('delete-btn')) {
+    loadingDOM.style.visibility = 'visible'
+    const id = el.parentElement.dataset.id
+    try {
+      await axios.delete(`/api/v1/tasks/${id}`)
+      showTasks()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  loadingDOM.style.visibility = 'hidden'
 })
 
 formDOM.addEventListener('submit', async (e) => {
