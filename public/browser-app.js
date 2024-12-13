8e9d4b56578b70d1f2428871d82ff61a0b11ad83
Nimira43 <n.rabone72@aol.com>
2024-12-13 23:44:14 +0000

    Generating HTML for each mapped task

--- public/browser-app.js
+++ public/browser-app.js
@@ -4,7 +4,37 @@ const formDOM = document.querySelector('task-form')
 const taskInputDOM = document.querySelector('task-input')
 const formAlertDOM = document.querySelector('form-alert')
 
-const showTasks = async () => {}
+const showTasks = async () => {
+  loadingDOM.style.visibility = 'visible'
+  try {
+    const {
+      data: { tasks },
+    } = await axios.get('/api/v1/tasks')
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = '<h5 class="empty-list">No task have been listed</h5>'
+      loadingDOM.style.visibility = 'hidden'
+      return    
+    }
+    const allTasks = tasks
+      .map((task) => {
+        const { completed, _id: taskID, name } = task
+        return `
+          <div class="single-task> ${completed && 'Task-completed'}">
+            <h5><span><i class="fa-regular fa-check-circle"></i></span>${name}</h5>
+            <div class="task-links">
+              <a href="task.html?id=${taskID}" class="edit-link">
+                <i class="fa-solid fa-edit"></i>
+              </a>
+              <button type="btn" class="delete-btn" data=-id="${taskID}">
+                <i className="fa-solid fa-trash"></i>
+              </button>
+            </div>
+        </div>
+           
+        `
+      })
+  } 
+}
 
 showTasks()
 
@@ -13,5 +43,5 @@ tasksDOM,addEventListener('click', async (e) => {
 })
 
 formDOM.addEventListener('submit', async (e) => {
-  
+
 })
