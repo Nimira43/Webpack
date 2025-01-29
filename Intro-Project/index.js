d429dca4a8b498e5a824bbd9eaaf272920c7eda0
Nimira43 <n.rabone72@aol.com>
2025-01-29 23:17:07 +0000

    Displaying basic list of movies.

--- Intro-Project/index.js
+++ Intro-Project/index.js
@@ -1,4 +1,13 @@
 function buttonClicked() {
   const el = document.getElementById('header')
   el.innerHTML = 'Update Completed!'
+
+  const listItems = ['Dune', 'Life of Brian', 'Skyfall']
+  const ul = document.getElementById('movie-list')
+  
+  _.forEach(listItems, function(item) {
+    const tempEl = document.createElement('li')
+    tempEl.innerHTML = item
+    ul.appendChild(tempEl)
+  })
 }
