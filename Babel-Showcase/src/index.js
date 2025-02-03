02547091041efa539b8d50c2f9604a87b581375d
Nimira43 <n.rabone72@aol.com>
2025-02-03 00:26:54 +0000

    Updated index.js

--- Babel-Showcase/src/index.js
+++ Babel-Showcase/src/index.js
@@ -1,31 +1,37 @@
-// Arrow Function
 const greet = (name) => `Hello, ${name}!`
 
 class Person {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
   greet() {
-    return greet(this.name)
+    return greet(this.name);
   }
 }
 
-// Async/Await Function
+const person = {
+  name: 'Ayrton',
+  address: {
+    city: 'Sao Paulo'
+  }
+}
+
+const city = person.address?.city ?? 'Unknown City'
+console.log(`City: ${city}`)
+
 const fetchData = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
-
+    
     const data = await response.json()
-
+    
     document.getElementById('data-output').innerText = JSON.stringify(data, null, 2)
   } catch (error) {
     document.getElementById('data-output').innerText = 'Error fetching data'
   }
 }
 
-// Event Listener
 document.getElementById('fetch-data').addEventListener('click', fetchData)
 
-// Create a Person instance and log greeting
-const person = new Person('Leonardo!')
-console.log(person.greet())
+const ayrton = new Person('Ayrton')
+console.log(ayrton.greet())
